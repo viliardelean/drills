d9364045852bd7e68a1aac2dbd57073d51aab1ec
viliardelean <willyardelean@gmail.com>
2019-07-15 19:47:59 +0300

    Inial commit

---
+++ src/main/java/com/ardelean/drills/App.java
@@ -0,0 +1,13 @@
+package com.ardelean.drills;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
